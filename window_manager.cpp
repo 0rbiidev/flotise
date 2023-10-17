6b9200da52b0e8a9ed9ea61f5bd7749d2ae02c5f
Jon <orbii@gmx.com>
2023-10-17 18:06:19 +0100

    finished WM skeleton

--- window_manager.cpp
+++ window_manager.cpp
@@ -2,3 +2,27 @@
 #include <glog/logging.h>
 
 using ::std::unique_ptr;
+
+unique_ptr<WindowManager> WindowManager::Create(){
+    // 1. Open X display
+
+    Display* display = XOpenDisplay(nullptr);
+    if (display == nullptr){
+        LOG(ERROR) <<"Failed to open X display" << XDisplayName(nullptr);
+        return nullptr;
+    }
+
+    // 2. Construct WM instance
+    return unique_ptr<WindowManager> (new WindowManager(display));
+}
+
+WindowManager::WindowManager(Display* display)
+    : display_(CHECK_NOTNULL(display)),
+      root_(DefaultRootWindow(display_)){
+}
+
+WindowManager::~WindowManager(){
+    XCloseDisplay(display_);
+}
+
+void WindowManager::Run() { /* TODO */}
